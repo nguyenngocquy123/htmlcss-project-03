f037e23ae02136e47af667bc82e203f0adc3c391
nguyenngocquy123 <ngocquynguyen2679@gmail.com>
2024-07-12 00:52:24 +0700

    First commit

---
+++ Javascript/main.js
@@ -0,0 +1,54 @@
+//Khai bao bien
+// var fullName = 'Nguyen Ngoc Quy';
+// var age = 30;
+
+//Goi ham thong bao
+// alert(fullName);
+// alert(age);
+
+/*
+1.alert
+2.console.log
+3.confirm
+4.prompt
+5.set timeout
+6.set interval
+*/
+
+//Su dung console.log: In ra
+//console.log(fullName)
+
+//Su dung confirm:Xac nhan
+//confirm('Xac nhan ban du tuoi');
+
+//Su dung prompt: Ra mot bang de nhap gia tri
+//prompt('Xac nhan du tuoi');
+
+//Su dung setTimeout: Chay dong thong bao sau 1 khoang thoi gian an dinh
+// setTimeout(function() {
+//     alert('Thong bao')
+// },1000)
+
+//Su dung setInterval: Chay dong thong bao 
+//lien tuc theo khoang thoi gian an dinh
+// setInterval(function() {
+//     console.log('Day la log')
+// },1000)
+
+/**
+ * Gioi thieu ve toan tu
+ * 1.Toan tu so hoc Arithmetic
+ * 2.Toan tu gan Assignment
+ * 3.Toan tu so sanh Comparison
+ * 4.Toan tu logic Logical
+ */
+
+//Toan tu so hoc Arithmetic
+// var a = 8;
+
+// a++;
+// a--;
+
+// console.log(a);
+
+//Toan tu gan Assignment
